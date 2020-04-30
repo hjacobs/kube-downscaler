87cd226b485b3d99b4935ba69a1d4c56c23e13f1
Henning Jacobs <henning@zalando.de>
2020-04-30 21:47:30 +0200

    #109 test force-uptime: false on namespace

--- tests/test_scaler.py
+++ tests/test_scaler.py
@@ -985,3 +985,66 @@ def test_scaler_name_excluded(monkeypatch):
     }
     assert api.patch.call_args[1]["url"] == "/deployments/deploy-2"
     assert json.loads(api.patch.call_args[1]["data"]) == patch_data
+
+
+def test_scaler_namespace_force_uptime_false(monkeypatch):
+    api = MagicMock()
+    monkeypatch.setattr(
+        "kube_downscaler.scaler.helper.get_kube_api", MagicMock(return_value=api)
+    )
+
+    def get(url, version, **kwargs):
+        if url == "pods":
+            data = {"items": []}
+        elif url == "deployments":
+            data = {
+                "items": [
+                    {
+                        "metadata": {
+                            "name": "deploy-1",
+                            "namespace": "ns-1",
+                            "creationTimestamp": "2019-03-01T16:38:00Z",
+                        },
+                        "spec": {"replicas": 1},
+                    },
+                ]
+            }
+        elif url == "namespaces/ns-1":
+            data = {"metadata": {"annotations": {"downscaler/force-uptime": "false"}}}
+        else:
+            raise Exception(f"unexpected call: {url}, {version}, {kwargs}")
+
+        response = MagicMock()
+        response.json.return_value = data
+        return response
+
+    api.get = get
+
+    include_resources = frozenset(["deployments"])
+    scale(
+        namespace=None,
+        upscale_period="never",
+        downscale_period="never",
+        default_uptime="never",
+        default_downtime="always",
+        include_resources=include_resources,
+        exclude_namespaces=[],
+        exclude_deployments=[],
+        dry_run=False,
+        grace_period=300,
+    )
+
+    assert api.patch.call_count == 1
+
+    # make sure that deploy-1 was updated
+    patch_data = {
+        "metadata": {
+            "name": "deploy-1",
+            "namespace": "default",
+            "creationTimestamp": "2019-03-01T16:38:00Z",
+            "annotations": {ORIGINAL_REPLICAS_ANNOTATION: "1"},
+        },
+        "spec": {"replicas": 0},
+    }
+    assert api.patch.call_args[1]["url"] == "/deployments/deploy-1"
+    assert json.loads(api.patch.call_args[1]["data"]) == patch_data
